3d1d615c64865592cee6fcadb43bb2551cdbe5d9
renatolmorais <renatolmorais@gmail.com>
2019-07-04 13:19:13 -0300

    api contatos. já verifica validade da requisição

---
+++ contatos.php
@@ -0,0 +1,43 @@
+<?php
+
+//var_dump($_REQUEST);
+//var_dump($_SERVER);
+
+include_once('include.php');
+
+switch($method)
+{
+	case 'GET':
+		$msg = "you want to get information about contact $id";
+		$status = "OK";
+		break;
+	case 'POST':
+		$msg = "you want to update information about contact $id";
+		$status = "OK";
+		break;
+	case 'PUT':
+		$msg = "you want to create contact $id";
+		$status = "OK";
+		break;
+	case 'DELETE':
+		$msg = "you want to delete contact $id";
+		$status = "OK";
+		break;
+	case 'HEAD':
+		$msg = "you want nothing";
+		$status = "OK";
+		break;
+}
+
+echo json_encode(
+	array(
+		"status" => $status,
+		"message" => $msg,
+		"response" => array(
+						"id" => $id,
+						"name" => ( array_key_exists('name',$input) ? $input['name'] : "" ),
+						"age" => ( array_key_exists('age',$input) ? $input['age'] : "" )
+					)
+	)
+);
+?>
